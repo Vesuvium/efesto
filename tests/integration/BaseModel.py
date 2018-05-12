38f45956526b021825818d85403cfef4b62b52a1
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-05-12 14:45:03 +0100

    tests(integration): fix Database integration tests

--- tests/integration/BaseModel.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from efesto.models.BaseModel import db
-
-
-def test_db_fallback():
-    assert db.database is None
-    assert list(db.connect_kwargs.values()) == [None, None, None]
