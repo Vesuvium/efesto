919f782489082a0ca8fd2241a9d9ce62bdef4344
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-07-02 22:14:00 +0200

    feat/config: use bassoon

--- tests/unit/Config.py
+++ tests/unit/Config.py
@@ -1,49 +1,16 @@
 # -*- coding: utf-8 -*-
-import os
+from bassoon import Bassoon
 
 from efesto.Config import Config
 
-from pytest import fixture
-
-
-@fixture
-def init(patch):
-    patch.object(Config, '__init__', return_value=None)
-
-
-@fixture
-def config():
-    return Config()
-
 
 def test_config():
+    assert issubclass(Config, Bassoon)
+
+
+def test_config_defaults():
     db_url = 'sqlite:///efesto.db'
     assert Config.defaults['db_url'] == db_url
     assert Config.defaults['jwt_secret'] == 'secret'
     assert Config.defaults['jwt_leeway'] == 5
     assert Config.defaults['jwt_audience'] == 'efesto'
-
-
-def test_config_init(patch):
-    patch.object(Config, 'apply')
-    Config()
-    Config.apply.assert_called_with()
-
-
-def test_config_apply(init):
-    config = Config()
-    config.defaults = {'one': 'value'}
-    config.apply()
-    assert config.one == 'value'
-
-
-def test_config_apply_from_env(patch, init):
-    patch.object(os, 'getenv', return_value='envvalue')
-    config = Config()
-    config.defaults = {'one': 'value'}
-    config.apply()
-    assert config.one == 'envvalue'
-
-
-def test_config_attribute_empty(config):
-    assert config.option is None
