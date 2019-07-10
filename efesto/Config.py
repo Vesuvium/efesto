bf624aa8b03013be0b0363206618138182336ce3
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-07-10 19:46:51 +0200

    feat/config: default LOG_LEVEL is info

--- efesto/Config.py
+++ efesto/Config.py
@@ -13,7 +13,7 @@ class Config(Bassoon):
         'JWT_SECRET': 'secret',
         'JWT_LEEWAY': '5',
         'JWT_AUDIENCE': 'efesto',
-        'LOG_LEVEL': 'error',
+        'LOG_LEVEL': 'info',
         'LOG_FORMAT': '[{time:YYYY-MM-DD HH:mm:ss}] [{level}] {message}',
         'PUBLIC_ENDPOINTS': 'index,version',
         'SWAGGER': '1',
--- tests/unit/Config.py
+++ tests/unit/Config.py
@@ -18,7 +18,7 @@ def test_config_defaults():
     assert Config.defaults['JWT_SECRET'] == 'secret'
     assert Config.defaults['JWT_LEEWAY'] == '5'
     assert Config.defaults['JWT_AUDIENCE'] == 'efesto'
-    assert Config.defaults['LOG_LEVEL'] == 'error'
+    assert Config.defaults['LOG_LEVEL'] == 'info'
     format = '[{time:YYYY-MM-DD HH:mm:ss}] [{level}] {message}'
     assert Config.defaults['LOG_FORMAT'] == format
     assert Config.defaults['PUBLIC_ENDPOINTS'] == 'index,version'
