9a76e5e8a5807efe5d4fb5858a07d8f01c773323
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-07-13 20:15:11 +0200

    feat/middlewares/json: call dumps with uuid_mode

--- efesto/middlewares/Json.py
+++ efesto/middlewares/Json.py
@@ -8,4 +8,4 @@ class Json:
         if success:
             if type(response.body) == dict:
                 response.body = rapidjson.dumps(response.body, datetime_mode=1,
-                                                number_mode=7)
+                                                number_mode=7, uuid_mode=1)
--- tests/unit/middlewares/Json.py
+++ tests/unit/middlewares/Json.py
@@ -8,7 +8,8 @@ def test_json_process_response(patch, response):
     patch.object(rapidjson, 'dumps')
     response.body = {}
     Json().process_response('request', response, 'res', True)
-    rapidjson.dumps.assert_called_with({}, datetime_mode=1, number_mode=7)
+    rapidjson.dumps.assert_called_with({}, datetime_mode=1, number_mode=7,
+                                       uuid_mode=1)
     assert response.body == rapidjson.dumps()
 
 
