ed85f33b617e874a52bf19f763826110b9f8c002
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-08-31 11:21:33 +0200

    chore/handlers/items: on_patch uses request.payload

--- tests/unit/handlers/Items.py
+++ tests/unit/handlers/Items.py
@@ -8,8 +8,6 @@ from peewee import DoesNotExist
 
 from pytest import fixture, raises
 
-import rapidjson
-
 
 @fixture
 def item(magic):
@@ -50,30 +48,26 @@ def test_items_on_get__not_found(patch, magic, http_request, item):
         item.on_get(http_request, 'response', user=user, id=1)
 
 
-def test_item_on_patch(patch, magic, item, siren):
-    patch.object(rapidjson, 'load')
-    request = magic()
-    response = magic()
+def test_item_on_patch(magic, response, http_request, item, siren):
     user = magic()
-    item.on_patch(request, response, user=user, id=1)
+    item.on_patch(http_request, response, user=user, id=1)
     item.model.select().where.assert_called_with(False)
     user.do.assert_called_with('edit', item.model.select().where(),
                                item.model)
-    user.do().get().edit.assert_called_with(rapidjson.load())
+    user.do().get().edit.assert_called_with(http_request.payload)
     siren.__init__.assert_called_with(item.model, user.do().get(),
-                                      request.path)
+                                      http_request.path)
     assert response.body == siren().encode()
 
 
-def test_item_on_patch__badrequest(patch, magic, http_request, item):
-    patch.object(rapidjson, 'load')
+def test_item_on_patch__badrequest(magic, http_request, item):
     user = magic()
     user.do().get().edit.return_value = None
     with raises(BadRequest):
         item.on_patch(http_request, 'response', user=user, id=1)
 
 
-def test_items_on_patch__notfound(patch, magic, http_request, item):
+def test_items_on_patch__notfound(magic, http_request, item):
     user = magic(do=magic(side_effect=DoesNotExist))
     with raises(NotFound):
         item.on_patch(http_request, 'response', user=user, id=1)
