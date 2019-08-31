2e77e069a93744d73ee2acfe367bc55269d4b138
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-08-31 02:22:52 +0200

    refactor/api: simplify method names by removing add_ prefix

--- efesto/Api.py
+++ efesto/Api.py
@@ -15,26 +15,26 @@ class Api:
         self.generator = Generator()
         self.api = None
 
-    def add_route(self, endpoint, handler, model=None):
+    def route(self, endpoint, handler, model=None):
         if model:
             return self.api.add_route(endpoint, handler(model))
         return self.api.add_route(endpoint, handler)
 
-    def add_routes(self, routes):
+    def routes(self, routes):
         for route in routes:
-            self.add_route(*route)
+            self.route(*route)
 
-    def add_custom_route(self, name, model):
+    def custom_route(self, name, model):
         endpoint = f'/{name}'
         items_endpoint = f'{endpoint}/{{id}}'
-        self.add_routes((
+        self.routes((
             (endpoint, Collections, model), (items_endpoint, Items, model)
         ))
 
-    def add_custom_routes(self, custom_types):
+    def custom_routes(self, custom_types):
         for custom_type in custom_types:
             model = self.generator.generate(custom_type)
-            self.add_custom_route(custom_type.name, model)
+            self.custom_route(custom_type.name, model)
 
     def middlewares(self):
         return [
@@ -49,6 +49,6 @@ class Api:
         Mounts the routes and starts the API
         """
         self.api = falcon.API(middleware=self.middlewares())
-        self.add_custom_routes(Types.select().execute())
-        self.add_routes(Routes.routes)
+        self.custom_routes(Types.select().execute())
+        self.routes(Routes.routes)
         return self.api
