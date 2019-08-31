e54b237c568fbb7e65fbbfcf582a28c652370abf
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-08-31 02:22:52 +0200

    refactor/api: simplify method names by removing add_ prefix

--- tests/unit/Api.py
+++ tests/unit/Api.py
@@ -23,41 +23,41 @@ def test_api_init(api, config):
     assert isinstance(api.generator, Generator)
 
 
-def test_api_add_route(magic, api):
+def test_api_route(magic, api):
     api.api = magic()
-    api.add_route('route', 'handler')
+    api.route('route', 'handler')
     api.api.add_route.assert_called_with('route', 'handler')
 
 
-def test_api_add_route__model(magic, api):
+def test_api_route__model(magic, api):
     handler = magic()
     api.api = magic()
-    api.add_route('route', handler, 'model')
+    api.route('route', handler, 'model')
     handler.assert_called_with('model')
     api.api.add_route.assert_called_with('route', handler())
 
 
-def test_api_add_routes(patch, api):
-    patch.object(Api, 'add_route')
-    api.add_routes((('endpoint', 'handler'), ))
-    Api.add_route.assert_called_with('endpoint', 'handler')
+def test_api_routes(patch, api):
+    patch.object(Api, 'route')
+    api.routes((('endpoint', 'handler'), ))
+    Api.route.assert_called_with('endpoint', 'handler')
 
 
-def test_api_add_custom_route(patch, api):
-    patch.object(Api, 'add_routes')
-    api.add_custom_route('name', 'model')
-    api.add_routes.assert_called_with((
+def test_api_custom_route(patch, api):
+    patch.object(Api, 'routes')
+    api.custom_route('name', 'model')
+    api.routes.assert_called_with((
         ('/name', Collections, 'model'), ('/name/{id}', Items, 'model')
     ))
 
 
-def test_api_add_custom_routes(patch, api, type_instance):
+def test_api_custom_routes(patch, api, type_instance):
     patch.object(Generator, 'generate')
-    patch.object(Api, 'add_custom_route')
-    api.add_custom_routes((type_instance, ))
+    patch.object(Api, 'custom_route')
+    api.custom_routes((type_instance, ))
     Generator.generate.assert_called_with(type_instance)
-    Api.add_custom_route.assert_called_with(type_instance.name,
-                                            Generator.generate())
+    Api.custom_route.assert_called_with(type_instance.name,
+                                        Generator.generate())
 
 
 def test_api_middlewares(patch, api, config):
@@ -76,9 +76,9 @@ def test_api_middlewares(patch, api, config):
 def test_api_start(patch, magic, api):
     patch.object(falcon, 'API')
     patch.object(Types, 'select')
-    patch.many(Api, ['add_custom_routes', 'add_routes', 'middlewares'])
+    patch.many(Api, ['custom_routes', 'routes', 'middlewares'])
     result = api.start()
     falcon.API.assert_called_with(middleware=Api.middlewares())
-    Api.add_custom_routes.assert_called_with(Types.select().execute())
-    Api.add_routes.assert_called_with(Routes.routes)
+    Api.custom_routes.assert_called_with(Types.select().execute())
+    Api.routes.assert_called_with(Routes.routes)
     assert result == api.api
