4f3aceea9ef3178e2f7f5f46f3e2409ac1b7dd47
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-09-01 01:41:48 +0200

    feat/middlewares/msgpack: add Msgpack middleware

--- efesto/middlewares/__init__.py
+++ efesto/middlewares/__init__.py
@@ -3,6 +3,7 @@ from .Authentication import Authentication
 from .Clacks import Clacks
 from .Json import Json
 from .Log import Log
+from .Msgpack import Msgpack
 
 
-__all__ = ['Authentication', 'Clacks', 'Json', 'Log']
+__all__ = ['Authentication', 'Clacks', 'Json', 'Log', 'Msgpack']
