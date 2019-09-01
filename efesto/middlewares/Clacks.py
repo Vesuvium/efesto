a37f4e0044cc55219f36a64eae634a4afe3072ea
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-09-01 02:06:59 +0200

    feat/middlewares/clacks: accepts config as constructor parameter

--- efesto/middlewares/Clacks.py
+++ efesto/middlewares/Clacks.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 class Clacks:
 
+    def __init__(self, config):
+        pass
+
     def process_response(self, request, response, resource, success):
         response.set_header('X-Clacks-Overhead', 'GNU Terry Pratchett')
--- tests/unit/middlewares/Clacks.py
+++ tests/unit/middlewares/Clacks.py
@@ -2,8 +2,12 @@
 from efesto.middlewares import Clacks
 
 
+def test_clacks_init():
+    assert Clacks('config')
+
+
 def test_clacks_process_response(response):
-    clacks = Clacks()
+    clacks = Clacks('config')
     clacks.process_response('request', response, 'resource', 'success')
     value = 'GNU Terry Pratchett'
     response.set_header.assert_called_with('X-Clacks-Overhead', value)
