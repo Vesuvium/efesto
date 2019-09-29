71a1f8144a59eee497d03d08088ed45674f599af
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-09-29 14:04:47 +0200

    feat/entrypoint: use poetry

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-efesto install;
+poetry run efesto install;
 
 if [ ! -z $ADMIN_NAME ]; then
-    efesto create users $ADMIN_NAME --superuser
+    poetry run efesto create users $ADMIN_NAME --superuser
 fi
 
 if [ ! -z "$BLUEPRINT" ]; then
     echo "$BLUEPRINT" > blueprint.yml;
-    efesto load blueprint.yml;
+    poetry run efesto load blueprint.yml;
 fi
 
 if [ ! $WORKERS ]; then
@@ -17,7 +17,7 @@ fi
 if [ ! $WORKER_CLASS ]; then
     WORKER_CLASS=sync;
 else
-    pip install $WORKER_CLASS;
+    poetry run pip install $WORKER_CLASS;
 fi
 
 if [ ! $THREADS ]; then
@@ -32,6 +32,6 @@ if [ ! $JITTER ]; then
     JITTER=$(( $WORKERS * 5 ));
 fi
 
-gunicorn "efesto.App:App.run()" -b :5000 -w $WORKERS -k $WORKER_CLASS \
-        --threads $THREADS -t $TIMEOUT --graceful-timeout $TIMEOUT \
-        --max-requests-jitter $JITTER
+poetry run gunicorn "efesto.App:App.run()" -b :5000 -w $WORKERS \
+        -k $WORKER_CLASS --threads $THREADS -t $TIMEOUT \
+        --graceful-timeout $TIMEOUT --max-requests-jitter $JITTER
