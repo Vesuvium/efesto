8e52594dc0e915c8e47b449ea2b3934d47c3d324
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-12-19 18:06:35 +0100

    chore/efesto: remove Database and Proxy modules

--- efesto/models/__init__.py
+++ efesto/models/__init__.py
@@ -14,10 +14,8 @@
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # -*- coding: utf-8 -*-
 from .Base import Base
-from .Database import db
 from .Fields import Fields
-from .Proxy import Proxy
 from .Types import Types
 from .Users import Users
 
-__all__ = ['db', 'Base', 'Fields', 'Proxy', 'Types', 'Users']
+__all__ = ['Base', 'Fields', 'Types', 'Users']
