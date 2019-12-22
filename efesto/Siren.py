4b75f42c043acc64bc0a52f202de65bdd7775333
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-12-22 23:06:48 +0100

    feat/siren: work with dictionaries from Psyker

--- efesto/Siren.py
+++ efesto/Siren.py
@@ -13,19 +13,11 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # -*- coding: utf-8 -*-
-from peewee import ModelSelect
 
 
 class Siren:
 
-    __slots__ = ('model', 'data', 'path', 'total', 'page')
-
-    def __init__(self, model=None, data=[], path='', total=0, page=1):
-        self.model = model
-        self.data = data
-        self.path = path
-        self.total = total
-        self.page = page
+    __slots__ = ()
 
     @staticmethod
     def paginate(path, items, current_page, total_items):
@@ -43,62 +35,57 @@ class Siren:
         return links
 
     @classmethod
-    def nested_entities(cls, include, item):
-        """
-        Parses nested entities. Used when a join is performed and thus
-        there are nested item to encode.
-        """
-        nested = getattr(item, include)
-        if isinstance(nested, ModelSelect):
-            nested = list(nested)
-            items = []
-            if len(nested) > 0:
-                path = '/{}'.format(nested[0].__class__.__name__)
-            for item in nested:
-                items.append(cls.entity(path, item))
-            return items
-        path = '/{}'.format(nested.__class__.__name__)
-        return cls.entity(path, nested)
-
-    @classmethod
-    def entity(cls, path, item, includes=[]):
+    def entity(cls, item, item_type, path, includes=[]):
         """
         Creates an entity from a model instance
         """
-        href = '{}/{}'.format(path, item.id)
-        if path.endswith('/{}'.format(item.id)):
+        href = f'{path}/{item["id"]}'
+        if path.endswith(f'/{item["id"]}'):
             href = path
 
         for include in includes:
-            item.__data__[include] = cls.nested_entities(include, item)
+            item[include] = [
+                cls.entity(n, include, f'/{include}') for n in item[include]
+            ]
 
         return {
-            'properties': item.__data__,
-            'class': [item.__class__.__name__],
+            'properties': item,
+            'class': [item_type.lower()],
             'links': [
                 {'href': href, 'rel': 'self'}
             ]
         }
 
-    def entities(self, includes=[]):
-        entities = []
-        for item in self.data:
-            entities.append(self.entity(self.path, item, includes=includes))
+    @classmethod
+    def field_type(cls, value):
+        try:
+            int(value)
+            return 'number'
+        except ValueError:
+            return 'text'
 
-        fields = []
-        name = 'add-item'
-        if self.model:
-            fields = self.model.get_columns()
-            name = 'add-' + self.model._meta.name
+    @classmethod
+    def fields(cls, data):
+        return [{key: cls.field_type(value)} for key, value in data[0].items()]
+
+    @classmethod
+    def entities(cls, data, item_type, path, includes, page, total):
+        entities = [
+            cls.entity(item, item_type, path, includes) for item in data
+        ]
+
+        fields = cls.fields(data)
+        name = f'add-{item_type}'
 
         actions = [
             {'name': name, 'method': 'POST', 'type': 'application/json',
              'fields': fields}
         ]
-        links = self.paginate(self.path, self.data, self.page, self.total)
+        links = cls.paginate(path, data, page, total)
         return {'entities': entities, 'actions': actions, 'links': links}
 
-    def encode(self, *args, includes=[]):
-        if isinstance(self.data, list):
-            return self.entities(includes=includes)
-        return self.entity(self.path, self.data, includes=includes)
+    @classmethod
+    def encode(cls, data, includes, item_type, path, page, total):
+        if type(data) == list:
+            return cls.entities(data, item_type, path, includes, page, total)
+        return cls.entity(data, item_type, path, includes)
--- tests/unit/Siren.py
+++ tests/unit/Siren.py
@@ -15,98 +15,86 @@
 # -*- coding: utf-8 -*-
 from efesto.Siren import Siren
 
-from pytest import fixture
-
-
-@fixture
-def siren():
-    return Siren()
+from pytest import mark
 
 
 def test_siren_paginate():
-    links = Siren().paginate('path', [], 1, 1)
-    assert links[0] == {'rel': ['self'], 'href': 'path'}
-    assert links[1] == {'href': 'path?page=2', 'rel': ['next']}
+    result = Siren.paginate('path', [], 1, 1)
+    assert result[0] == {'rel': ['self'], 'href': 'path'}
+    assert result[1] == {'href': 'path?page=2', 'rel': ['next']}
 
 
 def test_siren_paginate_middle():
-    links = Siren().paginate('path', [], 2, 1)
-    assert links[0] == {'href': 'path', 'rel': ['self']}
-    assert links[1] == {'href': 'path?page=3', 'rel': ['next']}
-    assert links[2] == {'href': 'path?page=1', 'rel': ['previous']}
+    result = Siren.paginate('path', [], 2, 1)
+    assert result[0] == {'href': 'path', 'rel': ['self']}
+    assert result[1] == {'href': 'path?page=3', 'rel': ['next']}
+    assert result[2] == {'href': 'path?page=1', 'rel': ['previous']}
 
 
 def test_siren_paginate_last():
-    links = Siren().paginate('path', [], 3, 0)
-    assert links[0] == {'rel': ['self'], 'href': 'path'}
-    assert links[1] == {'rel': ['previous'], 'href': 'path?page=2'}
+    result = Siren.paginate('path', [], 3, 0)
+    assert result[0] == {'rel': ['self'], 'href': 'path'}
+    assert result[1] == {'rel': ['previous'], 'href': 'path?page=2'}
 
 
-def test_nested_entities(patch, magic, siren):
-    patch.object(Siren, 'entity')
-    item = magic()
-    result = siren.nested_entities('include', item)
-    Siren.entity.assert_called_with('/MagicMock', item.include)
-    assert result == Siren.entity()
+def test_siren_entity():
+    item = {'id': 1}
+    result = Siren.entity(item, 'type', 'path', [])
+    assert result['properties'] == item
+    assert result['class'] == ['type']
+    assert result['links'] == [{'href': 'path/1', 'rel': 'self'}]
 
 
-def test_siren_entity(patch, magic, siren):
-    item = magic(__data__='data')
-    entity = siren.entity('', item)
-    assert entity['properties'] == item.__data__
-    assert entity['class'] == [item.__class__.__name__]
-    assert entity['links'] == [{'href': '/{}'.format(item.id), 'rel': 'self'}]
+def test_siren_entity__nested():
+    item = {'id': 1, 'nested': [{'id': 2}]}
+    result = Siren.entity(item, 'type', 'path', ['nested'])
+    expected = [Siren.entity({'id': 2}, 'nested', '/nested')]
+    assert result['properties']['nested'] == expected
 
 
-def test_siren_entity_nested(patch, magic, siren):
-    patch.object(Siren, 'nested_entities')
-    item = magic(__data__={'nested': 1})
-    entity = siren.entity('', item, includes=['nested'])
-    Siren.nested_entities.assert_called_with('nested', item)
-    assert entity['properties']['nested'] == Siren.nested_entities()
-
-
-def test_siren_entity_path(magic, siren):
-    item = magic(id=1, __data__='data')
-    entity = siren.entity('/endpoint/1', item)
+def test_siren_entity__path():
+    item = {'id': 1}
+    entity = Siren.entity(item, 'type', '/endpoint/1', [])
     assert entity['links'] == [{'href': '/endpoint/1', 'rel': 'self'}]
 
 
-def test_siren_make_entities(patch, magic, siren):
-    patch.many(Siren, ['entity', 'paginate'])
-    item = magic()
-    siren.data = [item]
-    entities = siren.entities()
-    Siren.entity.assert_called_with(siren.path, item, includes=[])
-    assert entities['entities'] == [Siren.entity()]
-    assert isinstance(entities['actions'], list)
-    assert entities['links'] == Siren.paginate()
+@mark.parametrize('value, expected', [
+    ('1', 'number'), (1, 'number'),
+    ('hello', 'text')
+])
+def test_siren_field_type(value, expected):
+    assert Siren.field_type(value) == expected
 
 
-def test_siren_make_entities_includes(patch, magic, siren):
-    patch.many(Siren, ['entity', 'paginate'])
-    item = magic()
-    siren.data = [item]
-    siren.entities(includes=['includes'])
-    Siren.entity.assert_called_with(siren.path, item, includes=['includes'])
+def test_siren_fields(patch):
+    patch.object(Siren, 'field_type')
+    result = Siren.fields([{'id': 1}])
+    Siren.field_type.assert_called_with(1)
+    assert result == [{'id': Siren.field_type()}]
 
 
-def test_siren_encode(patch, siren):
-    patch.object(Siren, 'entities')
-    result = siren.encode('string')
-    Siren.entities.assert_called_with(includes=[])
-    assert result == Siren.entities()
+def test_siren_entities(patch):
+    patch.many(Siren, ['entity', 'paginate', 'fields'])
+    data = [{'id': 1}]
+    result = Siren.entities(data, 'type', 'path', [], 1, 1)
+    Siren.entity.assert_called_with({'id': 1}, 'type', 'path', [])
+    Siren.fields.assert_called_with(data)
+    assert result['entities'] == [Siren.entity()]
+    assert result['actions'][0] == {'name': 'add-type', 'method': 'POST',
+                                    'type': 'application/json',
+                                    'fields': Siren.fields()}
+    assert result['links'] == Siren.paginate()
 
 
-def test_siren_encode__includes(patch):
-    patch.object(Siren, 'entities')
-    Siren().encode(includes=['includes'])
-    Siren.entities.assert_called_with(includes=['includes'])
-
-
-def test_siren_encode_one(patch, siren):
+def test_siren_encode(patch):
     patch.object(Siren, 'entity')
-    siren.data = {}
-    result = siren.encode('string')
-    Siren.entity.assert_called_with(siren.path, siren.data, includes=[])
+    result = Siren.encode('data', [], 'item_type', 'path', 1, 1)
+    Siren.entity.assert_called_with('data', 'item_type', 'path', [])
     assert result == Siren.entity()
+
+
+def test_siren_encode__entities(patch):
+    patch.object(Siren, 'entities')
+    result = Siren.encode(['data'], [], 'item_type', 'path', 1, 1)
+    Siren.entities.assert_called_with(['data'], 'item_type', 'path', [], 1, 1)
+    assert result == Siren.entities()
